23661f1026c30188ac5093790db4e58fd42b2e2b
anuragchhetri <anurag.chhetri@nagarro.com>
2024-01-30 11:45:01 +0530

    hello world

--- app/page.tsx
+++ app/page.tsx
@@ -25,20 +25,21 @@ export default function Home() {
  
   
   return (
-    <div className="flex">
-      <div className="w-1/5 text-white p-3 h-screen fixed" style={{ background: 'linear-gradient(180deg, rgba(39,16,78,1) 0%, rgba(100,55,159,1) 100%)' }}>
-        <main>
-          <h1>hi</h1>
-        </main>
-      </div>
-      <div className="w-4/5 flex-1 bg-yellow-300 ml-1/5" style={{marginLeft: '20%',height:'1200px'}}>
-        <Header />
-        <main>
-          <h1>hi</h1>
-          <button onClick={getWeatherData}>get</button>
+    // <div className="flex">
+    //   <div className="w-1/5 text-white p-3 h-screen fixed" style={{ background: 'linear-gradient(180deg, rgba(39,16,78,1) 0%, rgba(100,55,159,1) 100%)' }}>
+    //     <main>
+    //       <h1>hi</h1>
+    //     </main>
+    //   </div>
+    //   <div className="w-4/5 flex-1 bg-yellow-300 ml-1/5" style={{marginLeft: '20%',height:'1200px'}}>
+    //     <Header />
+    //     <main>
+    //       <h1>hi</h1>
+    //       <button onClick={getWeatherData}>get</button>
           
-        </main>
-      </div>
-    </div>
+    //     </main>
+    //   </div>
+    // </div>
+    <div>hello world</div>
   );
 }
